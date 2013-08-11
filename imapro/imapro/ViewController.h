03019506bb0f1206ad43689a0aa2188b5ea5baec
馬場 惇 <babajun@h15.95.242.10.104606.vlan.kuins.net>
2013-08-11 17:11:11 +0900

    画面作成

---
+++ imapro/imapro/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  imapro
+//
+//  Created by 馬場 惇 on 13/08/11.
+//  Copyright (c) 2013年 馬場 惇. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+@end
