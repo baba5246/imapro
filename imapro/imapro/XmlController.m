3b9470ccf7d779bfdafe4963705d7a041a8a8d15
baba5246 <baba_writer@yahoo.co.jp>
2013-10-16 04:07:56 +0900

    XML出力

--- imapro/imapro/XmlController.m
+++ imapro/imapro/XmlController.m
@@ -36,6 +36,34 @@
 -(IBAction)onExportXMLButtonClicked:(id)sender
 {
     model = [Model sharedManager];
+    
+    // XML作成
+    NSXMLDocument* document= [NSXMLNode document];
+    
+    [document setVersion:@"1.0"];
+    
+    NSXMLDTD* dtd= [[NSXMLDTD alloc] init];
+    [dtd setPublicID:@"__URL__"];
+    [document setDTD:dtd];
+    
+    [document addChild:[NSXMLNode commentWithStringValue:@"__Comment_String__"]];
+    
+    NSXMLElement* root= [NSXMLNode elementWithName:@"root"];
+    [document setRootElement:root];
+    
+    NSXMLElement* child= [NSXMLNode elementWithName:@"child"];
+    [root addChild:child];
+    
+    [child addAttribute:[NSXMLNode attributeWithName:@"attrKey" stringValue:@"attrVal"]];
+    
+    
+    // データ保存
+    NSSavePanel *save = [NSSavePanel savePanel];
+    if ([save runModal] == NSOKButton) {
+        NSURL * filePath = [save URL];
+        [[document XMLString] writeToFile:filePath.path atomically:YES encoding:4 error:NULL];
+    }
 }
+
 
 @end
